1cc5c20eb7f6c0313ecc6979e8549e99bbdd94e3
MariaG <MGrigorenko@bellintegrator.com>
2023-10-29 17:54:04 +0300

    login test

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/LoginTest.java
@@ -0,0 +1,94 @@
+package ru.iteco.fmhandroid.ui;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ru.iteco.fmhandroid.R;
+import ru.iteco.fmhandroid.ui.AppActivity;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(AppActivity.class);
+
+    @Test
+    public void loginTest() {
+        ViewInteraction textView = onView(
+                allOf(withText("Authorization"),
+                        withParent(withParent(withId(R.id.nav_host_fragment))),
+                        isDisplayed()));
+        textView.check(matches(withText("Authorization")));
+
+        ViewInteraction textInputEditText = onView(
+                allOf(childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.login_text_input_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("login2"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.password_text_input_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText2.perform(replaceText("password2"), closeSoftKeyboard());
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.trademark_image_view),
+                        withParent(allOf(withId(R.id.container_custom_app_bar_include_on_fragment_main),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
